c2bfc719fc73dd9bf3ca4b4f57088f4ecb6208cd
Vlada Sabelnikova <vladasabelnikova@gmail.com>
2022-01-05 14:30:35 +0300

    Работает правая лопатка, угол требует доработки
    Всё.

--- dinamic_obj/ball.py
+++ dinamic_obj/ball.py
@@ -32,8 +32,10 @@ class Ball(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
 
         # Рандомное направление вектора.
-        self.vx = -400 # random.randint(-2000, 2000)
-        self.vy = -400 # random.randint(-2000, -1500)
+        # self.vx = random.randint(-2000, 2000)
+        # self.vy = random.randint(-2000, -1500)
+        self.vx = random.randint(-500, 500)
+        self.vy = random.randint(-500, 500)
         self.x = x
         self.y = y
 
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dinamic_obj.ball import Ball
 from dinamic_obj.left_paddle import LeftPaddle
 # from dinamic_obj.left_paddle_bottom import LeftPaddleBottom
 # from dinamic_obj.left_paddle_top import LeftPaddleTop
+from dinamic_obj.right_paddle import RightPaddle
 from settings import KEY_LEFT, KEY_RIGHT
 from static_obj.wall import Wall
 from utils import load_image
@@ -83,7 +84,7 @@ def main():
 
     left_paddle_bottom = LeftPaddle(
         angle=334,
-        rebound_ratio=1,
+        rebound_ratio=0.91,
         img=load_image('sprites/paddle_bottom_left.png'),
         paddles=paddles,
         all_sprites=all_sprites,
@@ -94,13 +95,35 @@ def main():
 
     left_paddle_top = LeftPaddle(
         angle=133,
-        rebound_ratio=1,
+        rebound_ratio=0.91,
         img=load_image('sprites/paddle_top_left.png'),
         all_sprites=all_sprites,
         paddles=paddles,
         name='paddle_left_top',
         columns=31,  #!!!
         rows=1  #!!!
+    )
+
+    right_paddle_top = RightPaddle(
+        angle=223,
+        rebound_ratio=0.91,
+        img=load_image('sprites/paddle_top_right.png'),
+        all_sprites=all_sprites,
+        paddles=paddles,
+        name='paddle_right_top',
+        columns=31,  # !!!
+        rows=1  # !!!
+    )
+
+    right_paddle_bottom = RightPaddle(
+        angle=382,
+        rebound_ratio=0.91,
+        img=load_image('sprites/paddle_bottom_right.png'),
+        all_sprites=all_sprites,
+        paddles=paddles,
+        name='paddle_right_bottom',
+        columns=31,  # !!!
+        rows=1  # !!!
     )
 
     for i in range(1):
@@ -121,7 +144,8 @@ def main():
                     left_paddle_top.rotate_up = rotate
                     left_paddle_bottom.rotate_up = rotate
                 elif event.key == KEY_RIGHT:
-                    pass
+                    right_paddle_top.rotate_up = rotate
+                    right_paddle_bottom.rotate_up = rotate
 
             if event.type == pygame.KEYUP:
                 rotate = False
@@ -129,7 +153,8 @@ def main():
                     left_paddle_top.rotate_up = rotate
                     left_paddle_bottom.rotate_up = rotate
                 elif event.key == KEY_RIGHT:
-                    pass
+                    right_paddle_top.rotate_up = rotate
+                    right_paddle_bottom.rotate_up = rotate
 
         screen.fill((40, 40, 40))
         all_sprites.draw(screen)
