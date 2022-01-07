b05085c201f1643713d2c5816b1d1aa56f6f5274
Vlada Sabelnikova <vladasabelnikova@gmail.com>
2022-01-07 23:18:17 +0300

    Исправили настройки перед съемками
    Что туту еще нужно говорить?

--- main.py
+++ main.py
@@ -23,23 +23,23 @@ def main():
     blots = pygame.sprite.Group()
 
     Blot(
-        center_circle=(106, 100),
-        rebound_ratio=0.81,
-        img=load_image('sprites/blot_106_100.png'),
-        blots=blots,
-        all_sprites=all_sprites,
-        name='blot_106_100',
-        columns=9,
-        rows=2
-    )
-
-    Blot(
         center_circle=(250, 100),
         rebound_ratio=0.81,
         img=load_image('sprites/blot_250_100.png'),
         blots=blots,
         all_sprites=all_sprites,
         name='blot_250_100',
+        columns=9,
+        rows=2
+    )
+
+    Blot(
+        center_circle=(106, 100),
+        rebound_ratio=0.81,
+        img=load_image('sprites/blot_106_100.png'),
+        blots=blots,
+        all_sprites=all_sprites,
+        name='blot_106_100',
         columns=9,
         rows=2
     )
