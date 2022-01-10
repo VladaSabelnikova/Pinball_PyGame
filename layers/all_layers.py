817e9c493520b5e7727f14400c3aaeb36aa8aca6
Vlada Sabelnikova <vladasabelnikova@gmail.com>
2022-01-10 15:46:37 +0300

    Создали отдельный файл под игровой цикл
    В all_layers.py теперь идет импорт.

--- layers/all_layers.py
+++ layers/all_layers.py
@@ -1,14 +1,12 @@
-import pygame
-
 from dinamic_obj.ball import Ball
 from layers.all_blots import add_blots
 from layers.all_bumpers import add_lateral_bumper, add_center_bumper
 from layers.all_paddles import creation_paddles
 from layers.all_walls import add_walls
+from layers.game_loop import game_loop
 from layers.general_settings_for_game import creation_general_settings
-from settings import KEY_LEFT, KEY_RIGHT, BALL_PAUSE, SIMPLE_GRAVITY, \
-    AVERAGE_GRAVITY, NIGHTMARE_GRAVITY, EXTRA_BALLS, KEY_UP, KEY_DOWN
-from utils.lib import result_calculation, draw_paddle_speed, draw_number_balls
+from settings import SIMPLE_GRAVITY, \
+    AVERAGE_GRAVITY, NIGHTMARE_GRAVITY
 
 
 def simple():
@@ -19,7 +17,8 @@ def simple():
     add_blots(all_sprites, blots)
 
     left_paddle_top, right_paddle_top, \
-    left_paddle_bottom, right_paddle_bottom = creation_paddles(all_sprites,  paddles)
+    left_paddle_bottom, right_paddle_bottom = creation_paddles(all_sprites,
+                                                               paddles)
 
     ball = Ball(20, gravity=SIMPLE_GRAVITY)
 
@@ -50,7 +49,8 @@ def average():
     add_lateral_bumper(all_sprites, bumpers)
 
     left_paddle_top, right_paddle_top, \
-    left_paddle_bottom, right_paddle_bottom = creation_paddles(all_sprites,  paddles)
+    left_paddle_bottom, right_paddle_bottom = creation_paddles(all_sprites,
+                                                               paddles)
 
     ball = Ball(20, gravity=AVERAGE_GRAVITY)
 
@@ -103,94 +103,3 @@ def nightmare():
     )
 
     return result
-
-
-def game_loop(
-    all_sprites,
-    left_paddle_top,
-    left_paddle_bottom,
-    right_paddle_top,
-    right_paddle_bottom,
-    screen,
-    paddles,
-    walls,
-    blots,
-    bumpers,
-    shadow,
-    ball
-):
-    ball_pause = BALL_PAUSE
-    extra_balls = EXTRA_BALLS
-
-    speed_change = 0
-    skills_ratio = []
-
-    rotate = False
-    clock = pygame.time.Clock()
-    t = None
-    game_time = 0
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return None
-
-            if event.type == pygame.KEYDOWN:
-                rotate = True
-                if event.key == KEY_LEFT:
-                    left_paddle_top.rotate_up = rotate
-                    left_paddle_bottom.rotate_up = rotate
-                elif event.key == KEY_RIGHT:
-                    right_paddle_top.rotate_up = rotate
-                    right_paddle_bottom.rotate_up = rotate
-                if event.key == KEY_UP:
-                    speed_change = 3
-                if event.key == KEY_DOWN:
-                    speed_change = -3
-            if event.type == pygame.KEYUP:
-                rotate = False
-                if event.key == KEY_LEFT:
-                    left_paddle_top.rotate_up = rotate
-                    left_paddle_bottom.rotate_up = rotate
-                elif event.key == KEY_RIGHT:
-                    right_paddle_top.rotate_up = rotate
-                    right_paddle_bottom.rotate_up = rotate
-                if event.key == KEY_UP or event.key == KEY_DOWN:
-                    speed_change = 0
-        screen.fill((40, 40, 40))
-        all_sprites.draw(screen)
-        draw_number_balls(screen, extra_balls)
-        draw_paddle_speed(screen, left_paddle_top.speed)
-        t = clock.tick() / 1000
-        game_time += t
-
-        if speed_change:
-            left_paddle_top.set_speed(speed_change)
-            left_paddle_bottom.set_speed(speed_change)
-            right_paddle_top.set_speed(speed_change)
-            right_paddle_bottom.set_speed(speed_change)
-
-        all_blots_broken = all([blot.broken for blot in blots.sprites()])
-        if not ball.groups() and not all_blots_broken:
-            ball_pause -= t
-            if ball_pause <= 0:
-                skills_ratio.append(left_paddle_top.speed)
-                ball_pause = BALL_PAUSE
-                ball.position_creation()
-                ball.add(all_sprites)
-        if all_blots_broken:  # победа
-            ball_pause -= t
-            if ball_pause <= 0:
-                return result_calculation(game_time, extra_balls, skills_ratio)
-        if ball.y >= 700:  # лузер
-            ball_pause -= t
-            if ball_pause <= 0:
-                if extra_balls:
-                    ball_pause = BALL_PAUSE
-                    extra_balls -= 1
-                    ball.position_creation()
-                else:
-                    return -1
-        all_sprites.update(paddles, walls, blots, bumpers, t, all_sprites,
-                           shadow, screen)
-        pygame.display.flip()
-    pygame.quit()
---
+++ layers/game_loop.py
@@ -0,0 +1,96 @@
+import pygame
+
+from settings import KEY_LEFT, KEY_RIGHT, BALL_PAUSE, EXTRA_BALLS, KEY_UP, \
+    KEY_DOWN
+from utils.lib import result_calculation, draw_paddle_speed, draw_number_balls
+
+
+def game_loop(
+    all_sprites,
+    left_paddle_top,
+    left_paddle_bottom,
+    right_paddle_top,
+    right_paddle_bottom,
+    screen,
+    paddles,
+    walls,
+    blots,
+    bumpers,
+    shadow,
+    ball
+):
+    ball_pause = BALL_PAUSE
+    extra_balls = EXTRA_BALLS
+
+    speed_change = 0
+    skills_ratio = []
+
+    rotate = False
+    clock = pygame.time.Clock()
+    t = None
+    game_time = 0
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return None
+
+            if event.type == pygame.KEYDOWN:
+                rotate = True
+                if event.key == KEY_LEFT:
+                    left_paddle_top.rotate_up = rotate
+                    left_paddle_bottom.rotate_up = rotate
+                elif event.key == KEY_RIGHT:
+                    right_paddle_top.rotate_up = rotate
+                    right_paddle_bottom.rotate_up = rotate
+                if event.key == KEY_UP:
+                    speed_change = 3
+                if event.key == KEY_DOWN:
+                    speed_change = -3
+            if event.type == pygame.KEYUP:
+                rotate = False
+                if event.key == KEY_LEFT:
+                    left_paddle_top.rotate_up = rotate
+                    left_paddle_bottom.rotate_up = rotate
+                elif event.key == KEY_RIGHT:
+                    right_paddle_top.rotate_up = rotate
+                    right_paddle_bottom.rotate_up = rotate
+                if event.key == KEY_UP or event.key == KEY_DOWN:
+                    speed_change = 0
+        screen.fill((40, 40, 40))
+        all_sprites.draw(screen)
+        draw_number_balls(screen, extra_balls)
+        draw_paddle_speed(screen, left_paddle_top.speed)
+        t = clock.tick() / 1000
+        game_time += t
+
+        if speed_change:
+            left_paddle_top.set_speed(speed_change)
+            left_paddle_bottom.set_speed(speed_change)
+            right_paddle_top.set_speed(speed_change)
+            right_paddle_bottom.set_speed(speed_change)
+
+        all_blots_broken = all([blot.broken for blot in blots.sprites()])
+        if not ball.groups() and not all_blots_broken:
+            ball_pause -= t
+            if ball_pause <= 0:
+                skills_ratio.append(left_paddle_top.speed)
+                ball_pause = BALL_PAUSE
+                ball.position_creation()
+                ball.add(all_sprites)
+        if all_blots_broken:  # победа
+            ball_pause -= t
+            if ball_pause <= 0:
+                return result_calculation(game_time, extra_balls, skills_ratio)
+        if ball.y >= 700:  # лузер
+            ball_pause -= t
+            if ball_pause <= 0:
+                if extra_balls:
+                    ball_pause = BALL_PAUSE
+                    extra_balls -= 1
+                    ball.position_creation()
+                else:
+                    return -1
+        all_sprites.update(paddles, walls, blots, bumpers, t, all_sprites,
+                           shadow, screen)
+        pygame.display.flip()
+    pygame.quit()
