7964cbfe28e92b03047549d5c135f1daa0d55aa9
Vlada Sabelnikova <vladasabelnikova@gmail.com>
2022-01-11 02:32:04 +0300

    all_paddles.py Продакшн. Всё.

--- layers/all_paddles.py
+++ layers/all_paddles.py
@@ -1,9 +1,24 @@
+from typing import Tuple
+
+import pygame.sprite
+
 from dinamic_obj.left_paddle import LeftPaddle
 from dinamic_obj.right_paddle import RightPaddle
 from utils.lib import load_image
 
 
-def creation_paddles(all_sprites, paddles):
+def creation_paddles(
+    all_sprites: pygame.sprite.Group,
+    paddles: pygame.sprite.Group
+) -> Tuple[LeftPaddle, RightPaddle, LeftPaddle, RightPaddle]:
+    """
+    Функция создаст и вернёт все части лопаток.
+
+    :param all_sprites: группа со всеми спрайтами
+    :param paddles: группа со всеми частями лопаток
+    :return: Вернёт именованные спрайты, части лопаток.
+    """
+
     left_paddle_top = LeftPaddle(
         angle=133,
         rebound_ratio=0.96,
@@ -11,8 +26,8 @@ def creation_paddles(all_sprites, paddles):
         all_sprites=all_sprites,
         paddles=paddles,
         name='paddle_left_top',
-        columns=15,  # !!!
-        rows=2  # !!!
+        columns=15,
+        rows=2
     )
 
     right_paddle_top = RightPaddle(
@@ -22,8 +37,8 @@ def creation_paddles(all_sprites, paddles):
         all_sprites=all_sprites,
         paddles=paddles,
         name='paddle_right_top',
-        columns=15,  # !!!
-        rows=2  # !!!
+        columns=15,
+        rows=2
     )
 
     left_paddle_bottom = LeftPaddle(
@@ -33,8 +48,8 @@ def creation_paddles(all_sprites, paddles):
         paddles=paddles,
         all_sprites=all_sprites,
         name='paddle_left_bottom',
-        columns=15,  # !!!
-        rows=2  # !!!
+        columns=15,
+        rows=2
     )
 
     right_paddle_bottom = RightPaddle(
@@ -44,8 +59,12 @@ def creation_paddles(all_sprites, paddles):
         all_sprites=all_sprites,
         paddles=paddles,
         name='paddle_right_bottom',
-        columns=15,  # !!!
-        rows=2  # !!!
+        columns=15,
+        rows=2
     )
 
-    return left_paddle_top, right_paddle_top, left_paddle_bottom, right_paddle_bottom
+    return \
+        left_paddle_top, \
+        right_paddle_top, \
+        left_paddle_bottom, \
+        right_paddle_bottom
