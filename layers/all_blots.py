e27ba9b2d38195dbcfaa88e2242f24d593c35a30
AlexSeliv-22 <selivanovaaleksa@yandex.ru>
2022-01-11 02:06:01 +0300

    Закомментированы all_blots.py и all_bumpers.py
    Всё

--- layers/all_blots.py
+++ layers/all_blots.py
@@ -1,8 +1,19 @@
+import pygame.sprite
+
 from dinamic_obj.blot import Blot
 from utils.lib import load_image
 
 
-def add_blots(all_sprites, blots):
+def add_blots(
+    all_sprites: pygame.sprite.Group,
+    blots: pygame.sprite.Group
+) -> None:
+    """
+    Функция создаёт все цветные банки
+    :param all_sprites: группа со всеми спрайтами
+    :param blots: группа со всеми цветными банками
+    :return: Банки создаются безымянными, в программе нигде не вызываются
+    """
     Blot(
         center_circle=(250, 100),
         rebound_ratio=0.81,
--- layers/all_bumpers.py
+++ layers/all_bumpers.py
@@ -1,8 +1,20 @@
+import pygame.sprite
+
 from settings import BUMP_REBOUND_RATIO_LATERAL, BUMP_REBOUND_RATIO_CENTER
 from static_obj.bumper import Bumper
 
 
-def add_center_bumper(all_sprites, bumpers):
+def add_center_bumper(
+    all_sprites: pygame.sprite.Group,
+    bumpers: pygame.sprite.Group
+) -> None:
+    """
+    Функция создаёт центральный отбойник
+    :param all_sprites: группа со всеми спрайтами
+    :param bumpers: группа со всеми отбойниками
+    :return: Создаст центральный бампер, чтобы можно было вставить его
+    на уровень отдельно от остальных.
+    """
     Bumper(
         center_circle=[250, 330],
         img='images/bumper_center_250_330.png',
@@ -13,7 +25,17 @@ def add_center_bumper(all_sprites, bumpers):
     )
 
 
-def add_lateral_bumper(all_sprites, bumpers):
+def add_lateral_bumper(
+    all_sprites: pygame.sprite.Group,
+    bumpers: pygame.sprite.Group
+) -> None:
+    """
+    Функция создаёт боковые отбойники
+    :param all_sprites: группа со всеми спрайтами
+    :param bumpers: группа со всеми отбойниками
+    :return: Создаст боковые отбойники, чтобы можно было вставить их
+    на уровень отдельно от остальных.
+    """
     Bumper(
         center_circle=[-270, 307],
         img='images/bumper_left_-270_307.png',
