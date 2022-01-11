45f47b47de050345b07ebe00d4b0a0dc19706fd5
Vlada Sabelnikova <vladasabelnikova@gmail.com>
2022-01-11 16:35:48 +0300

    main.py Готов.
    Всё.

--- main.py
+++ main.py
@@ -8,13 +8,19 @@ layers = [simple, average, nightmare]
 
 def main():
     while True:
+        # name_layer — выбор пользователя о том, какой уровень ему хочется.
         name_layer = start()
-        if name_layer:
+        if name_layer:  # Если не закрыл окно
             layer_id = ID_LAYERS[name_layer]
             layer = layers[layer_id]
+
+            # score — кол-во набранных очков в уровне
             score = layer()
-            if score:
+            if score:  # Если не закрыл окно
                 user_choice = stop(score, layer_id)
+
+                # user_choice — выбор пользователя о том,
+                # продолжаем ли мы играть или же выходим из игры.
                 if user_choice:
                     if user_choice == 'Закончить':
                         break
